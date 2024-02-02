6a5c1d9908a19f9e5dbe103e3b1a80f497f89d67
kimhj365 <khj6428@gmail.com>
2024-02-02 17:56:44 +0900

    20240202 Vue.js 과제 답

---
+++ vue/BodyComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul id="myUL">
+    <li v-for="todo in todos" v-bind:key="todo.no" v-bind:class="{checked: todo.cancelFlag}"
+      @click="cancelTodo(todo.no)">{{todo.todo}} <span @click="deleteTodo(todo.no)" class="close">×</span></li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    props: ['todos'],
+    methods: {
+      cancelTodo(no) {
+        this.$emit('cancel-todo', no);
+      },
+      deleteTodo(no) {
+        this.$emit('delete-todo', no);
+      }
+    }
+  }
+</script>
---
+++ vue/HeadComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="myDIV" class="header">
+    <h2 style="margin:5px">My To Do List</h2>
+    <input type="text" v-model="myInput" placeholder="Title...">
+    <span v-on:click="newElement()" class="addBtn">Add</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        myInput: ''
+      }
+    },
+    methods: {
+      newElement() {
+        this.$emit('add-todo', this.myInput);
+        this.myInput = '';
+      }
+    }
+  }
+</script>
